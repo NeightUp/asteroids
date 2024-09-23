08d20bd51c297c5dc1e92fa83a7b694c17b280b6
NeightUp <picturen8@yahoo.com>
2024-09-23 04:30:47 -0400

    added player and circle classes

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import pygame as p
+import pygame
 from constants import *
+from player import *
 
 def main():
-    p.init()
-    screen = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        for event in p.event.get():
-            if event.type == p.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
-        p.display.flip()
+        ship.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
