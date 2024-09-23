ca87200eb5a075040ce52dccddbd13160da6daec
NeightUp <picturen8@yahoo.com>
2024-09-23 05:55:46 -0400

    added groups

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -15,10 +19,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        for u in updatable:
+            u.update(dt)
+
         screen.fill("black")
-        ship.update(dt)
-        ship.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
